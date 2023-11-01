a488c53e055d6a3fb6e1a189a2f9a1ac9e42492b
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-11-01 22:28:17 +0330

    add data and send obj(not)

--- src/components/CreatNote.jsx
+++ src/components/CreatNote.jsx
@@ -1,17 +1,40 @@
 import styles from "./CreatNote.module.css";
+import { listsNote } from "../data";
+import { useState } from "react";
 function CreatNote({ handlerPage }) {
+  const [listData, setListData] = useState(listsNote);
+  // useEffect(() => setListData(listsNote), []);
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function handlerInput(event) {
+    setEnteredTitle(event.target.value);
+  }
+  function submitData(event) {
+    event.preventDefault();
+    const data = {
+      title: enteredTitle,
+      decriptionOne:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five",
+      decriptiontwo:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also…",
+      id: Math.random().toString(),
+    };
+    const neWrefrence = [...listsNote];
+    setListData(neWrefrence.push(data));
+    console.log(listData);
+  }
   return (
     <div className={styles.containerCreatNote}>
       <div className={styles.iconContainer}>
         <button onClick={() => handlerPage("home")} className={styles.coIcon}>
-          <i class="bi bi-chevron-compact-left"></i>
+          <i className="bi bi-chevron-compact-left"></i>
         </button>
-        <button className={styles.coIcon}>
-          <i class="bi bi-floppy"></i>
+        <button onClick={submitData} className={styles.coIcon}>
+          <i className="bi bi-floppy"></i>
         </button>
       </div>
       <div className={styles.inputTxt}>
-        <input type="text" placeholder="Title" />
+        <input onChange={handlerInput} type="text" placeholder="Title" />
         <p className={styles.txt}>Type something....</p>
       </div>
     </div>
