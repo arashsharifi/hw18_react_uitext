8c76f3792dc426e8eb506989328796138da43e94
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-11-02 09:54:51 +0330

    feat:done delete data and add

--- src/components/CreatNote.jsx
+++ src/components/CreatNote.jsx
@@ -1,8 +1,7 @@
 import styles from "./CreatNote.module.css";
-import { listsNote } from "../data";
+
 import { useState } from "react";
-function CreatNote({ handlerPage }) {
-  const [listData, setListData] = useState(listsNote);
+function CreatNote({ handlerPage, onGetDataSend }) {
   // useEffect(() => setListData(listsNote), []);
   const [enteredTitle, setEnteredTitle] = useState("");
 
@@ -19,9 +18,7 @@ function CreatNote({ handlerPage }) {
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also…",
       id: Math.random().toString(),
     };
-    const neWrefrence = [...listsNote];
-    setListData(neWrefrence.push(data));
-    console.log(listData);
+    onGetDataSend(data);
   }
   return (
     <div className={styles.containerCreatNote}>
